663eefb673d14d37333b888629d7bcdd4cd26570
DESKTOP-0FPKCKK\Jacob <jakubjwerner@gmail.com>
2019-10-05 10:59:17 +0200

    player grid movement

---
+++ LD45Project/Assets/Grid/Scripts/PlayGrid.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGrid : MonoBehaviour
+{
+    [SerializeField] private float tileSize;
+    [SerializeField] private Vector2 gridSize;
+    private Vector2Int gridPoints;
+    //tiles states
+    public enum TileStatus { Empty, Free, Blocked }
+    private TileStatus[,] tiles;
+    //objects in grid
+    public List<GameObject> inGridObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //setup grid
+        gridPoints.x = (int)((gridSize.x - transform.position.x) / tileSize);
+        gridPoints.y = (int)((gridSize.y - transform.position.y) / tileSize);
+        tiles = new TileStatus[gridPoints.x, gridPoints.y];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        //draw grid boundaries
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position + new Vector3(gridSize.x / 2, gridSize.y / 2), new Vector2(gridSize.x, gridSize.y));
+    }
+
+    #region GET_SET
+
+    public float getTileSize() { return tileSize; }
+    public Vector2 getGridSize() { return gridSize; }
+    public Vector2Int getGridPoints() { return gridPoints; }
+
+    #endregion
+
+    #region GRID_HANDLING
+
+    //setup
+    public void SetupGridTiles()
+    {
+        //set every tile to empty
+        for(int x = 0; x < tiles.GetLength(0); x++)
+        {
+            for (int y = 0; y < tiles.GetLength(0); y++)
+            {
+                tiles[x, y] = TileStatus.Empty;
+            }
+        }
+
+        //check for object in grid
+        foreach(GameObject go in inGridObjects)
+        {
+            //check if is in grid
+            Vector2Int goPoint = getPointFromPosition(go.transform.position);
+            if (goPoint.x > 0 &&  goPoint.x <= gridPoints.x && goPoint.y > 0 && goPoint.y <= gridPoints.y)
+            {
+                //not blocking way
+
+                //blocking way
+            }
+        }
+    }
+
+    //get actual state of tiles in play grid
+    public void UpdateGridTiles()
+    {
+        foreach (TileStatus tile in tiles)
+        {
+            
+        }
+    }
+
+    #endregion
+
+    #region GRID_ORIENTATION
+
+    //return vector of tile point in grid
+    public Vector2Int getPointFromPosition(Vector2 position)
+    {
+        int x = (int)((position.x - transform.position.x) / tileSize);
+        int y = (int)((position.y - transform.position.y) / tileSize);
+
+        return new Vector2Int(x, y);
+    }
+
+    //return position from given coordinates of point
+    public Vector3 getGridPositionFromPoint(Vector2Int point)
+    {
+        float x = transform.position.x + (point.x * tileSize);
+        float y = transform.position.y + (point.y * tileSize);
+
+        return new Vector2(x, y);
+    }
+
+    //return if position is free to move
+    public TileStatus getTileStatusFromPoint(Vector2Int point)
+    {
+        TileStatus tile = tiles[point.x, point.y];
+        return tile;
+    }
+
+    #endregion
+}
---
+++ LD45Project/Assets/Player/Scripts/Player.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //movement properties
+    [SerializeField] private int moveSpeed;
+    [SerializeField] private bool isMoveingFree;
+    public enum PlayerMovement { Stand, Up, Down, Right, Left }
+    private PlayerMovement actualMovement;
+    public Vector2Int pointStand, pointDestination;
+
+    //references
+    [SerializeField] private PlayGrid playGrid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get components
+        //get references
+
+        //setup
+        pointStand = new Vector2Int(0, 0);
+        pointDestination = new Vector2Int(1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveAlongGrid();
+    }
+
+    //make player movement bounded to grid
+    void MoveAlongGrid()
+    {
+        //get input
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //handle movement
+        switch (actualMovement)
+        {
+            //player is not moving
+            case PlayerMovement.Stand:
+                //horizontal movemnt
+                Vector2Int mp = getMovingPoint(h, v);
+                if (pointStand.x + mp.x > 0 && pointStand.x + mp.x < playGrid.getGridPoints().x && pointStand.y + mp.y > 0 && pointStand.y + mp.y < playGrid.getGridPoints().y)
+                pointDestination = pointStand + mp;
+                //another move
+                if (mp.x == 1)
+                    actualMovement = PlayerMovement.Right;
+                else if (mp.x == -1)
+                    actualMovement = PlayerMovement.Left;
+                else if (mp.y == 1)
+                    actualMovement = PlayerMovement.Up;
+                else if (mp.y == -1)
+                    actualMovement = PlayerMovement.Down;
+                break;
+            //player is heading up
+            case PlayerMovement.Up:
+                //go back down
+                if (v < 0)
+                {
+                    pointDestination.y = pointStand.y;
+                    pointStand.y++;
+                    actualMovement = PlayerMovement.Down;
+                }
+                break;
+            //player is heading down
+            case PlayerMovement.Down:
+                //go back up
+                if (v > 0)
+                {
+                    pointDestination.y = pointStand.y;
+                    pointStand.y--;
+                    actualMovement = PlayerMovement.Up;
+                }
+                break;
+            //player is heading right
+            case PlayerMovement.Right:
+                //go back left
+                if (h < 0)
+                {
+                    pointDestination.x = pointStand.x;
+                    pointStand.x++;
+                    actualMovement = PlayerMovement.Left;
+                }
+                break;
+            //player is heading left
+            case PlayerMovement.Left:
+                //go back left
+                if (h > 0)
+                {
+                    pointDestination.x = pointStand.x;
+                    pointStand.x--;
+                    actualMovement = PlayerMovement.Right;
+                }
+                break;
+        }
+
+        //movement requare to have cubes in its way
+        if (!isMoveingFree)
+        {
+            if (playGrid.getTileStatusFromPoint(new Vector2Int(pointDestination.x, pointDestination.y)) != PlayGrid.TileStatus.Blocked)
+            {
+
+            }
+        }
+        //free movemnt on grid - for testing and some special situations only
+        else
+        {
+            if (playGrid.getTileStatusFromPoint(new Vector2Int(pointDestination.x, pointDestination.y)) == PlayGrid.TileStatus.Free)
+            {
+
+            }
+        }
+
+
+        //do the move if is not on position
+        if(transform.position != playGrid.getGridPositionFromPoint(pointDestination))
+        {
+            Vector3 vec = playGrid.getGridPositionFromPoint(pointDestination);
+            Vector3 headingPos = new Vector3(vec.x, vec.y, vec.z);
+            transform.position = Vector3.MoveTowards(transform.position, headingPos, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //update stand point on arive
+            pointStand = pointDestination;
+            actualMovement = PlayerMovement.Stand;
+        }
+    }
+
+    Vector2Int getMovingPoint(float x, float y)
+    {
+        Vector2Int finalMove = new Vector2Int(0, 0);
+
+        //horizontal
+        if (y == 0f)
+        {
+            if (x > 0)
+            {
+                finalMove = new Vector2Int(1, 0);
+            }
+            else if (x < 0)
+            {
+                finalMove = new Vector2Int(-1, 0);
+            }
+        }
+        else
+        {
+            //vertical
+            if (y > 0)
+            {
+                finalMove = new Vector2Int(0, 1);
+            }
+            else if (y < 0)
+            {
+                finalMove = new Vector2Int(0, -1);
+            }
+        }
+
+        return finalMove;
+    }
+}
